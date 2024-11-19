af35ba3a3f7b161bae3bca5ebadc274c1304b476
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2024-11-19 23:01:19 +0000

    users: use traditional linux user/group assignment

--- nixos/user.nix
+++ nixos/user.nix
@@ -5,9 +5,10 @@
       users = {
         ${username} = {
           inherit uid;
-          shell = pkgs.zsh;
+          group = "${username}";
           openssh.authorizedKeys.keys = [ gpg.sshKey ];
           ignoreShellProgramCheck = true; # Shell is set by Home Manager
+          shell = pkgs.zsh;
           homeMode = "750";
           isNormalUser = true;
           description = fullName;
