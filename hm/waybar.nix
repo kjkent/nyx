51d9986e85931594ad5ea7c23f224569ffef9f40
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2024-12-08 19:37:59 +0000

    waybar: style changes

--- hm/waybar.nix
+++ hm/waybar.nix
@@ -1,79 +1,65 @@
 {
-  pkgs,
+  config,
   lib,
+  nixosUser,
   osConfig,
+  pkgs,
   ...
 }:
 let
-  transition = "all 0.3s cubic-bezier(.55,-0.68,.48,1.682)";
   clock24h = true;
 in
 with lib;
+        
 {
   config = {
+    
     # Configure & Theme Waybar
     programs.waybar = {
       enable = true;
-      package = pkgs.waybar;
       settings = [
         {
           layer = "top";
           position = "top";
           modules-center = [ "hyprland/workspaces" ];
           modules-left = [
-            "custom/startmenu"
-            "hyprland/window"
+            "idle_inhibitor"
             "pulseaudio"
             "cpu"
             "memory"
-            "idle_inhibitor"
+            "disk"
+            "custom/music"
           ];
           modules-right = [
-            "custom/hyprbindings"
-            "custom/notification"
-            "custom/exit"
+            "backlight"
+            "custom/notifications"
             "battery"
             "tray"
             "clock"
+            "custom/exit"
           ];
 
           "hyprland/workspaces" = {
             format = "{name}";
-            format-icons = {
-              default = " ";
-              active = " ";
-              urgent = " ";
-            };
             on-scroll-up = "hyprctl dispatch workspace e+1";
             on-scroll-down = "hyprctl dispatch workspace e-1";
           };
-          "clock" = {
-            format = if clock24h then ''  {:L%H:%M}'' else ''  {:L%I:%M %p}'';
-            tooltip = true;
+          clock = {
+            format = if clock24h then '' {:L%H:%M}'' else '' {:L%I:%M %p}'';
             tooltip-format = "<big>{:%A, %d.%B %Y }</big>\n<tt><small>{calendar}</small></tt>";
           };
-          "hyprland/window" = {
-            max-length = 22;
-            separate-outputs = false;
-            rewrite = {
-              "" = "(V)(o;;;o)(V)";
-            };
-          };
-          "memory" = {
+          memory = {
             interval = 5;
-            format = "  {}%";
-            tooltip = true;
+            format = " {}%";
           };
-          "cpu" = {
+          cpu = {
             interval = 5;
-            format = "  {usage:2}%";
-            tooltip = true;
+            format = " {usage:2}%";
           };
-          "disk" = {
-            format = "  {free}";
-            tooltip = true;
+          disk = {
+            format = " {percentage_used}%";
           };
-          "network" = {
+          network = {
             format-icons = [
               "󰤯"
               "󰤟"
@@ -82,19 +68,27 @@ with lib;
               "󰤨"
             ];
             format-ethernet = " {bandwidthDownOctets}";
-            format-wifi = "{icon} {signalStrength}%";
+            format-wifi = "{icon}{signalStrength}%";
             format-disconnected = "󰤮";
+          };
+          tray = {
+            spacing = 10;
+          };
+          "custom/music" = {
+            format = "  {}";
+            escape = true;
+            interval = 5;
             tooltip = false;
+            exec = "${pkgs.playerctl}/bin/playerctl metadata --format='{{ title }}'";
+            on-click = "${pkgs.playerctl}/bin/playerctl play-pause";
+            max-length = 50;
           };
-          "tray" = {
-            spacing = 12;
-          };
-          "pulseaudio" = {
+          pulseaudio = {
             format = "{icon} {volume}% {format_source}";
             format-bluetooth = "{volume}% {icon} {format_source}";
             format-bluetooth-muted = "󰝟 {icon} {format_source}";
             format-muted = "󰝟 {format_source}";
-            format-source = " {volume}%";
+            format-source = "  {volume}%";
             format-source-muted = "";
             format-icons = {
               headphone = "";
@@ -111,31 +105,23 @@ with lib;
             };
             on-click = "pavucontrol";
           };
-          "custom/exit" = {
-            tooltip = false;
-            format = "";
-            on-click = "wlogout";
-          };
-          "custom/startmenu" = {
-            tooltip = false;
-            format = "";
-            on-click = "rofi -show drun";
-          };
-          "custom/hyprbindings" = {
-            tooltip = false;
-            format = "󱕴";
-            on-click = "list-bindings";
-          };
-          "idle_inhibitor" = {
+          idle_inhibitor = {
             format = "{icon}";
             format-icons = {
               activated = "󰈈";
               deactivated = "󰒲";
             };
-            tooltip = "true";
           };
-          "custom/notification" = {
-            tooltip = false;
+          backlight = {
+            device = "intel_backlight";
+            format = "{icon}";
+            format-icons = ["" "" "" "" "" "" "" "" ""];
+          }; 
+          "custom/exit" = {
+            on-click = "loginctl terminate-user ${builtins.toString osConfig.users.users.${nixosUser.username}.uid}";
+            format = " 󰩈 ";
+          };
+          "custom/notifications" = {
             format = "{icon} {}";
             format-icons = {
               notification = " <span foreground='red'><sup></sup></span>";
@@ -153,9 +139,9 @@ with lib;
             on-click = "sleep 0.1 && wl-task-waybar";
             escape = true;
           };
-          "battery" = {
+          battery = {
             states = {
-              warning = 30;
+              warning = 20;
               critical = 15;
             };
             format = "{icon} {capacity}%";
@@ -173,85 +159,138 @@ with lib;
               "󰂂"
               "󰁹"
             ];
-            on-click = "";
-            tooltip = false;
           };
         }
       ];
-      style = concatStrings [
-        ''
-          * {
-            font-family: ${osConfig.stylix.fonts.monospace.name};
-            font-size: 16px;
-            border-radius: 0px;
-            border: none;
-            min-height: 0px;
-          }
-          window#waybar {
-            background: rgba(0,0,0,0);
-          }
-          #workspaces {
-            margin: 4px 4px;
-            padding: 5px 5px;
-            border-radius: 16px;
-          }
-          #workspaces button {
-            font-weight: bold;
-            padding: 0px 5px;
-            margin: 0px 3px;
-            border-radius: 16px;
-            opacity: 0.5;
-            transition: ${transition};
-          }
-          #workspaces button.active {
-            font-weight: bold;
-            padding: 0px 5px;
-            margin: 0px 3px;
-            border-radius: 16px;
+      style = ''
+        /*
+        *
+        * Catppuccin Mocha palette
+        * Maintainer: rubyowo
+        *
+        */
+        
+        @define-color base   #1e1e2e;
+        @define-color mantle #181825;
+        @define-color crust  #11111b;
+        
+        @define-color text     #cdd6f4;
+        @define-color subtext0 #a6adc8;
+        @define-color subtext1 #bac2de;
+        
+        @define-color surface0 #313244;
+        @define-color surface1 #45475a;
+        @define-color surface2 #585b70;
+        
+        @define-color overlay0 #6c7086;
+        @define-color overlay1 #7f849c;
+        @define-color overlay2 #9399b2;
+        
+        @define-color blue      #89b4fa;
+        @define-color lavender  #b4befe;
+        @define-color sapphire  #74c7ec;
+        @define-color sky       #89dceb;
+        @define-color teal      #94e2d5;
+        @define-color green     #a6e3a1;
+        @define-color yellow    #f9e2af;
+        @define-color peach     #fab387;
+        @define-color maroon    #eba0ac;
+        @define-color red       #f38ba8;
+        @define-color mauve     #cba6f7;
+        @define-color pink      #f5c2e7;
+        @define-color flamingo  #f2cdcd;
+        @define-color rosewater #f5e0dc;
+        
+        label.module, #workspaces button {
+          border-radius: 0% 100% 0% 100% / 80%;
+          font-family: BerkeleyMono Nerd Font;
+          font-size: 18px;
+          margin: 0px 6px;
+          padding: 0px 4px;
+          min-height: 0;
+          background-color: @mantle;
+        }
+        
+        window#waybar {
+          margin-top: 2px;
+          background: transparent;
+          color: @text;
+        }
+        
+        #workspaces button {
+          color: @lavender;
+        }
+        
+        #workspaces button.active {
+          color: @teal;
+        }
+        
+        #workspaces button:hover {
+          color: @pink;
+        }
+        
+        #clock {
+          color: @blue;
+        }
+        
+        #battery {
+          color: @green;
+        }
 
+        #cpu {
+          color: @maroon;
+        }
+        #memory {
+          color: @peach;
+        }
+        #disk {
+          color: @yellow;
+        }
+        
+        #battery.charging {
+          color: @green;
+        }
 
-            transition: ${transition};
-            opacity: 1.0;
-            min-width: 40px;
+        #idle_inhibitor {
+          color: @lavender;
+        }
+
+        @keyframes blink {
+          to {
+            background-color: @red;
+            color: @mantle;
           }
-          #workspaces button:hover {
-            font-weight: bold;
-            border-radius: 16px;
-            opacity: 0.8;
-            transition: ${transition};
-          }
-          tooltip {
-            border-radius: 12px;
-          }
-          #window, #pulseaudio, #cpu, #memory, #idle_inhibitor {
-            font-weight: bold;
-            margin: 4px 0px;
-            margin-left: 7px;
-            padding: 0px 18px;
-            border-radius: 24px 10px 24px 10px;
-          }
-          #custom-startmenu {
-            font-size: 28px;
-            margin: 0px;
-            padding: 0px 30px 0px 15px;
-            border-radius: 0px 0px 40px 0px;
-          }
-          #custom-hyprbindings, #network, #battery,
-          #custom-notification, #tray, #custom-exit {
-            font-weight: bold;
-            margin: 4px 0px;
-            margin-right: 7px;
-            border-radius: 10px 24px 10px 24px;
-            padding: 0px 18px;
-          }
-          #clock {
-            font-weight: bold;
-            margin: 0px;
-            padding: 0px 15px 0px 30px;
-            border-radius: 0px 0px 0px 40px;
-          }
-        ''
-      ];
+        }
+        
+        #battery.warning:not(.charging) {
+          color: @red;
+          animation-name: blink;
+          animation-duration: 0.5s;
+          animation-timing-function: steps(12);
+          animation-iteration-count: infinite;
+          animation-direction: alternate;
+        }
+        
+        #backlight {
+          color: @sky;
+        }
+        
+        #pulseaudio {
+          color: @red;
+        }
+        
+        #custom-music {
+          color: @green;
+        }
+
+        #custom-notifications {
+          color: @sapphire;
+        }
+        
+        #custom-exit {
+            color: @mauve;
+        }
+      '';
     };
     home.packages = [
       (pkgs.writeShellScriptBin "wl-task-waybar" ''
