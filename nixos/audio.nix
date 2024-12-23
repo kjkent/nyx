7e21751f39816bd3c1b84bf79b502560661b5f15
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2024-12-23 23:57:01 +0000

    spotify

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 {
   config = {
+    boot = {
+      extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+      kernelModules = [ "v4l2loopback" ];
+    };
+
     environment.systemPackages = with pkgs; [
       ardour
       audacity
@@ -14,14 +19,32 @@
       v4l-utils
       vlc
     ];
-    boot = {
-      extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-      kernelModules = [ "v4l2loopback" ];
-    };
+
     hardware.pulseaudio.enable = false;
+
+    nixpkgs.overlays = [
+      (post: pre: with pre; let
+        spotx = fetchFromGitHub {
+          owner = "SpotX-Official";
+          repo = "SpotX-Bash";
+          rev = "fa0ea870b878fa38d6aa33d61d4191d3854d0e71";
+          hash = "sha256-/KGH8pKG2KNfo8m49bEk0PKYAS21YdCVUeMHunZH4fA=";
+        };
+        in {
+          spotify = spotify.overrideAttrs (pkg: {
+            buildInputs = [perl unzip zip];
+            postInstall = (pkg.postInstall or "") + ''
+              bash ${spotx}/spotx.sh -P $out/share/spotify/ -c
+            '';
+          });
+        }
+      )
+    ];
+
     programs = {
       dconf.enable = true;
     };
+
     services = {
       pipewire = {
         enable = true;
