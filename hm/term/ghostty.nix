743f1289a05d30a1e0a056df38e6caf29f9c71d4
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-01-14 15:38:51 +0000

    Add hm ghostty module

--- hm/term/ghostty.nix
+++ hm/term/ghostty.nix
@@ -1,18 +1,38 @@
-{pkgs, ...}: let
-  ghostty = pkgs.unstable.ghostty;
+{config, lib, pkgs, ...}: let
 in {
   config = {
-    home.packages = [ ghostty ];
     programs = {
-      bat.syntaxes.ghostty = {
-        src = ghostty;
-        file = "share/bat/syntaxes/ghostty.sublime-syntax";
+      ghostty = {
+        enable = true;
+        clearDefaultKeybinds = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        installVimSyntax = true;
+        package = pkgs.unstable.ghostty; 
+        settings = {
+          keybind = [
+            "ctrl+t>c=new_tab"
+            "ctrl+t>p=previous_tab"
+            "ctrl+t>n=next_tab"
+            "ctrl+t>/=last_tab"
+
+            "ctrl+s>s=new_split:auto"
+            "ctrl+s>h=new_split:left"
+            "ctrl+s>j=new_split:down"
+            "ctrl+s>k=new_split:up"
+            "ctrl+s>l=new_split:right"
+            "ctrl+s>e=equalize_splits"
+
+            "ctrl+h=goto_split:left"
+            "ctrl+j=goto_split:bottom"
+            "ctrl+k=goto_split:top"
+            "ctrl+l=goto_split:right"
+
+            "ctrl+q=close_surface"
+            "ctrl+shift+q=close_all_windows"
+          ];
+        };
       };
-      zsh.initExtra = ''
-        if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
-          source "$GHOSTTY_RESOURCES_DIR"/shell-integration/zsh/ghostty-integration
-        fi
-      '';
     };
   };
 }
