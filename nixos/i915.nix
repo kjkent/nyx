66719e6cd0d7f6a6118c6e7b199d09ffa7a204d3
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-01-22 15:38:09 +0000

    i915 tweaks

--- nixos/i915.nix
+++ nixos/i915.nix
@@ -5,30 +5,29 @@ with pkgs; {
       initrd.kernelModules = ["i915"];
       kernelParams = [
         "i915.enable_guc=2"
-        "i915.enable_fbc=1"
-        # PSR:
-        # 0 == ...
-        # 2 == awful flickering
-        # 3 == less awful flickering
+        "i915.enable_fbc=0"
         "i915.enable_psr=0"
+        "i915.fastboot=1"
       ];
     };
     environment = {
       sessionVariables = {
         LIBVA_DRIVER_NAME = "iHD";
       };
-      systemPackages = [
-        libva-utils
-      ];
     };
     hardware.graphics = {
       enable = true;
       extraPackages = [
         intel-compute-runtime
+        intel-gmmlib
+        intel-gpu-tools
         intel-media-driver
+        intel-media-sdk
         intel-ocl
-        vpl-gpu-rt
+        libva
         libvdpau-va-gl
+        libvpl
+        vpl-gpu-rt
       ];
       extraPackages32 = with pkgsi686Linux; [intel-media-driver];
     };
