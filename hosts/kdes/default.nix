9cca1272bcd39b33a65c6a8a71243cfaf650a161
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-01-31 14:15:43 +0000

    Update monitor layout

--- hosts/kdes/default.nix
+++ hosts/kdes/default.nix
@@ -9,7 +9,7 @@
     networking.networkBridge.enable = true;
     programs.hyprland.monitors = [
       "HDMI-A-1,2560x1080@60,0x0,1"
-      "DP-1,1920x1080@75,-1920x-310,1"
+      "DP-1,1920x1080@75,-1920x10,1"
     ];
   };
 }
