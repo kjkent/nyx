88c1ee450474ed55ee09e0fbc83fbde1252adeb2
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-02-13 19:24:51 +0000

    Add sysrq enable

--- nixos/kernel.nix
+++ nixos/kernel.nix
@@ -3,12 +3,13 @@
     boot = {
       kernelPackages = pkgs.linuxPackages_zen;
       kernel.sysctl = {
+        "kernel.sysrq" = 1; # Enable all SysRq functions. https://docs.kernel.org/admin-guide/sysrq.html
+        "net.core.rmem_max" = 7500000; # Recommended by QUIC docs
+        "net.core.wmem_max" = 7500000;
         # Needed for some Steam games
         "vm.max_map_count" = 2147483642;
         # Some server/db software tweaks
         "vm.overcommit_memory" = 1;
-        "net.core.rmem_max" = 7500000; # Recommended by QUIC docs
-        "net.core.wmem_max" = 7500000;
       };
     };
   };
