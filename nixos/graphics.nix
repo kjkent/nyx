8f16a806edf1d432a57d966e93f6cbe40ce7ff8f
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-02-16 14:41:30 +0000

    add ddcutil + ddcui

--- nixos/graphics.nix
+++ nixos/graphics.nix
@@ -2,6 +2,8 @@
   config = {
     environment.systemPackages = with pkgs; [
       clinfo
+      ddcui
+      ddcutil
       gpu-viewer
       libva-utils
       mesa-demos # for glxgears, eglinfo, glxinfo
@@ -12,5 +14,24 @@
         enable = true;
       };
     };
+    #nixpkgs.overlays = [(_: pre: {
+    #  # Use VCS main branch for ddcutil/ddcui as no release for 1yr+
+    #  ddcutil = pre.ddcutil.overrideAttrs (_: {
+    #    src = pre.fetchFromGitHub {
+    #      owner = "rockowitz";
+    #      repo = "ddcutil";
+    #      rev = "2.2.0-dev";
+    #      hash = "sha256-TJWxNeVa4UAY4jkeETi2wrCMtpzrwHiGKc1ABPFZImg=";
+    #    };
+    #  });
+    #  ddcui = pre.ddcutil.overrideAttrs (_: {
+    #    src = pre.fetchFromGitHub {
+    #      owner = "rockowitz";
+    #      repo = "ddcui";
+    #      rev = "0.5.5-dev";
+    #      hash = "sha256-8QW9XO08OJwRXNIoktPp3Z2IdaVZ3zq/s1wJPbIHW8Y=";
+    #    };
+    #  });
+    #})];
   };
 }
