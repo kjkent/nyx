eedb0c86d6fbbc6ab1ec083c84458cee909a00c9
Kristopher James Kent (kjkent) <kris@kjkent.dev>
2025-02-19 17:48:47 +0000

    earlyoom: remove options have changed, and I don`t want to find out what they are now. Plus, nix build OOM is dealt with by systemd-oomd, so earlyoom is kinda surplus to requirements

--- nixos/ram.nix
+++ nixos/ram.nix
@@ -12,37 +12,6 @@
         tmpfsSize = "30%";
       };
     };
-    # https://discourse.nixos.org/t/nix-build-ate-my-ram/35752
-    services.earlyoom = {
-      enable = true;
-      enableNotifications = true;
-      extraArgs = let
-        catPatterns = patterns: builtins.concatStringsSep "|" patterns;
-        preferPatterns = [
-          "firefox" # .firefox-unwrapped
-          "clang" # clang++
-          "java" # java + .java-unwrapped //"If it's written in java it's uninmportant enough it's ok to kill it"
-          "^ninja$"
-          "^nix$"
-          "^node$"
-        ];
-        avoidPatterns = [
-          "bash"
-          "dbus"
-          "docker"
-          "gpg"
-          "kworker"
-          "server"
-          "ssh"
-          "systemd"
-          "tmux"
-          "zsh"
-        ];
-      in [
-        "--prefer '${catPatterns preferPatterns}'"
-        "--avoid '${catPatterns avoidPatterns}'"
-      ];
-    };
     swapDevices = [];
     zramSwap.enable = true;
   };
